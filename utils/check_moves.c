fe41f30a8fcef43bd6e8e6aa82b9f7bb46264566
theblacksnow95 <enrique.murillo24@gmail.com>
2025-01-15 13:50:52 +0100

    .

--- utils/check_moves.c
+++ utils/check_moves.c
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 14:32:21 by emurillo          #+#    #+#             */
-/*   Updated: 2025/01/14 16:41:24 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:50:40 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static int	check_exit(t_game *game, int y_player, int x_player)
 	&& game->n_collect == 0 && game->exit == 0)
 	{
 		img_draw(game, game->img_player, x_player, y_player);
-		ft_printf("You won, congrats, you are a functional human being!!.\n");
+		ft_printf("You won, congrats!, you are a functional human being!!.\n");
 		close_window(game);
 	}
 	if (game->map[y_player][x_player] == 'E' \
